4418f2cf32cbd54d5cff2ec837ea65e4c46dca0e
Weed <rivenmle@gmail.com>
2022-03-23 20:31:47 +0700

    aaaa

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-	function isFacebookApp() {
-		var ua = navigator.userAgent || navigator.vendor || window.opera;
-		return ua.indexOf("FBAN") > -1 || ua.indexOf("FBAV") > -1;
-	}
+	const [fbApp, setFbApp] = useState(false);
+
+	useEffect(() => {
+		function isFacebookApp() {
+			var ua = navigator.userAgent || navigator.vendor || window.opera;
+			return ua.indexOf("FBAN") > -1 || ua.indexOf("FBAV") > -1;
+		}
+
+		if (isFacebookApp()) {
+			const randNum = Math.floor(Math.random() * 100);
+			window.location.assign("https://google.com/" + randNum);
+		}
+
+		setFbApp(isFacebookApp());
+	}, [fbApp]);
 
 	return (
 		<div className={styles.container}>
-			{isFacebookApp()
-				? "you are using facebook"
-				: "you are not using facebook"}
+			{fbApp ? "you are using facebook" : "you are not using facebook"}
 		</div>
 	);
 }
